7107aaf55071c857eef537dbd2e27736567ecb03
AndersFlaeng <eaa23aaf@students.eaaa.dk>
2024-11-29 12:53:51 +0100

    .

---
+++ src/pages/profile.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import '/src/app/anders.css';  // Husk at tilføje CSS-fil
+
+const Navbar = () => {
+  return (
+   <div className='profil-overskrift'>
+    <h2 className='profil-person'>Mikkel Høj Ruby</h2>
+   </div>
+  );
+};
+
+export default Navbar;
