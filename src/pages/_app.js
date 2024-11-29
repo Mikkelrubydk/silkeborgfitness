010582955024276f52bf41ecec1fc416c1e4ed1b
MikkelRuby <mikkelruby1@hotmail.com>
2024-11-29 12:51:15 +0100

    push

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "../app/globals.css";
+
+function MyApp({}) {
+  return <></>;
+}
+
+export default MyApp;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,3 @@
-export default function Home() {
-  return (
-    <>
-      <h1>hejsa</h1>
-    </>
-  );
+export default function Home({}) {
+  return <></>;
 }
