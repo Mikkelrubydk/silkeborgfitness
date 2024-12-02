83a0ce5f91698d6b5a442329002385f252dc2603
MikkelRuby <mikkelruby1@hotmail.com>
2024-12-02 09:46:31 +0100

    n

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,44 @@
 import "/src/app/anders.css"; // Husk at tilføje CSS-fil
 import Image from "next/image";
+import { useRouter } from "next/router";
 import logo from "/public/logo.svg";
+import arrow from "/public/arrow.svg";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  // Vælg billede baseret på den aktuelle rute
+  const imageSrc = router.pathname === "/" ? logo : arrow;
+
   return (
     <div className="navbar">
       <div>
         <Image
           className="navbar-pil-tilbage"
-          src={logo}
-          alt="SilkeborgFitness logo"
+          src={imageSrc}
+          alt={
+            router.pathname === "/" ? "SilkeborgFitness logo" : "Tilbage pil"
+          }
           width={200}
           height={200}
+          onClick={() => {
+            if (router.pathname !== "/") {
+              router.back();
+            }
+          }}
         />
       </div>
       <div>
         <h2 className="navbar-overskrift">Træningsoversigt</h2>
       </div>
       <div>
-        <img className="navbar-profilbillede" src="" alt="" />
+        <Image
+          className="navbar-profilbillede"
+          src=""
+          alt="Profilbillede"
+          width={100}
+          height={100}
+        />
       </div>
     </div>
   );
