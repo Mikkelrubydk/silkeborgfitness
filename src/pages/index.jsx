df019c19b1ddeba3dcea7c51da1210c449a7f39b
MikkelRuby <mikkelruby1@hotmail.com>
2024-12-04 10:54:13 +0100

    fiks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,47 +1,3 @@
-import '/src/app/Julie.css';
-import '/src/app/globals.css';
-
-export default function Home() {
-  return (
-    <>
-<div className="forside_container">
-      {/* Øverste række */}
-      <div className="forside_box forside_logbog">
-  <div className="top-left">Logbog</div>
-  <div className="center">
-    <img src="dumbbell.png" alt="Logbog Billede" />
-  </div>
-  <div className="bottom-left">Sidst noteret: 29/11</div>
-</div>
-
-
-      <div className="forside_box forside_skridt">11,238 Skridt</div>
-
-      {/* Midterste række */}
-      <div className="forside_box forside_praestationer">
-  <div className="top-left">Præstationer</div>
-  <div className="center">
-    <img src="medal.png" alt="Præstationer Billede" />
-  </div>
-  <div className="bottom-left">Ny præmie låst op!</div>
-</div>
-
-
-      <div className="forside_box forside_graf">Oversigt over travlhed</div>
-
-      {/* Nederste række */}
-      <div className="forside_box forside_tutorials">
-  <div className="top-left">Tutorials</div>
-  <div className="center">
-    <img src="video.png" alt="Tutorials Billede" />
-  </div>
-</div>
-
-
-      <div className="forside_box forside_fremskridt">Fremskridt</div>
-    </div>
-    </>
-  );
 import { useEffect, useState } from "react";
 import { db, auth } from "../lib/firebase"; // Husk at importere db og auth
 import { doc, getDoc } from "firebase/firestore"; // Importer Firestore funktioner
@@ -101,5 +57,38 @@ export default function Home() {
     localStorage.setItem("theme", theme);
   }, [theme]); // Kør denne effect, hver gang temaet ændres
 
-  return <main></main>;
+  return (
+    <main>
+      <div className="forside_container">
+        <div className="forside_box forside_logbog">
+          <div className="top-left">Logbog</div>
+          <div className="center">
+            <img src="dumbbell.png" alt="Logbog Billede" />
+          </div>
+          <div className="bottom-left">Sidst noteret: 29/11</div>
+        </div>
+
+        <div className="forside_box forside_skridt">11,238 Skridt</div>
+
+        <div className="forside_box forside_praestationer">
+          <div className="top-left">Præstationer</div>
+          <div className="center">
+            <img src="medal.png" alt="Præstationer Billede" />
+          </div>
+          <div className="bottom-left">Ny præmie låst op!</div>
+        </div>
+
+        <div className="forside_box forside_graf">Oversigt over travlhed</div>
+
+        <div className="forside_box forside_tutorials">
+          <div className="top-left">Tutorials</div>
+          <div className="center">
+            <img src="video.png" alt="Tutorials Billede" />
+          </div>
+        </div>
+
+        <div className="forside_box forside_fremskridt">Fremskridt</div>
+      </div>
+    </main>
+  );
 }
