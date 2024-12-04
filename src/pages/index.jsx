35db8301c09ad8a546099103106c5f0c189fd810
Julie Eskildsen <144682521+Julieeskildsen@users.noreply.github.com>
2024-12-04 10:47:39 +0100

    Julie

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,45 @@
+import '/src/app/Julie.css';
+import '/src/app/globals.css';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+<div className="forside_container">
+      {/* Øverste række */}
+      <div className="forside_box forside_logbog">
+  <div className="top-left">Logbog</div>
+  <div className="center">
+    <img src="dumbbell.png" alt="Logbog Billede" />
+  </div>
+  <div className="bottom-left">Sidst noteret: 29/11</div>
+</div>
+
+
+      <div className="forside_box forside_skridt">11,238 Skridt</div>
+
+      {/* Midterste række */}
+      <div className="forside_box forside_praestationer">
+  <div className="top-left">Præstationer</div>
+  <div className="center">
+    <img src="medal.png" alt="Præstationer Billede" />
+  </div>
+  <div className="bottom-left">Ny præmie låst op!</div>
+</div>
+
+
+      <div className="forside_box forside_graf">Oversigt over travlhed</div>
+
+      {/* Nederste række */}
+      <div className="forside_box forside_tutorials">
+  <div className="top-left">Tutorials</div>
+  <div className="center">
+    <img src="video.png" alt="Tutorials Billede" />
+  </div>
+</div>
+
+
+      <div className="forside_box forside_fremskridt">Fremskridt</div>
+    </div>
+    </>
+  );
 }
