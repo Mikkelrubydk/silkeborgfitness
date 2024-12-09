1ad16a010da96ae70192d2e8587d0b3805e8310f
Julie Eskildsen <144682521+Julieeskildsen@users.noreply.github.com>
2024-12-09 13:56:20 +0100

    julie

--- src/pages/machines.jsx
+++ src/pages/machines.jsx
@@ -1,11 +1,39 @@
-import { db, auth } from "../lib/firebase"; // Husk at importere db og auth
-import { doc, getDoc } from "firebase/firestore"; // Importer Firestore funktioner
+import { db, auth } from "../lib/firebase";  // Husk at importere db og auth
+import { doc, getDoc } from "firebase/firestore";  // Importer Firestore funktioner
 import Link from "next/link";
+import "/src/app/Julie.css";
+import "/src/app/globals.css";
 
 export default function About() {
     return (
-      <div className="benpress">
-      
+      <div>
+        {/* Benpress div */}
+        <div className="benpress">
+          <img src="benpress.png" alt="Benpress image" />
+          <div className="bp"></div> {/* Grøn div nedenunder */}
+          <p>Benpress</p>
+        </div>
+    
+        {/* Squat div */}
+        <div className="squat">
+          <img src="squat.png" alt="Squat image" />
+          <div className="sq"></div> {/* Grøn div nedenunder */}
+          <p>Squat</p>
+        </div>
+  
+        {/* Pullup div */}
+        <div className="pullup">
+          <img src="pullup.png"/>
+          <div className="pu"></div> {/* Grøn div nedenunder */}
+          <p>Pullup</p>
+        </div>
+  
+        {/* Bænkpress div */}
+        <div className="benchpress">
+          <img src="benchpress.png" alt="Bænkpress image" />
+          <div className="bp"></div> {/* Grøn div nedenunder */}
+          <p>Bænkpress</p>
+        </div>
       </div>
     );
   }
