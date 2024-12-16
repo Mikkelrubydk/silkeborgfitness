535d9e0cd30e5e1f73a9ca6e64f8d43337341378
MikkelRuby <mikkelruby1@hotmail.com>
2024-12-16 14:19:49 +0100

    ret

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   basePath: "/silkeborgfitness",
   reactStrictMode: true,
   trailingSlash: true,
-  // Tilføj også assetPrefix for at sikre korrekt håndtering af statiske filer
-  assetPrefix: "/silkeborgfitness/",
 };
 
 export default nextConfig;
