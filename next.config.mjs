fc318dce5b3b8dd4daeed8112208d49e51ecb167
MikkelRuby <mikkelruby1@hotmail.com>
2024-12-16 11:17:35 +0100

    fiks

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/sileborgfitness",
-  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true, // Deaktiver billedoptimering
