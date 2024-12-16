e1767ca75c3bc827413c2e68bc5de91dd6ea3116
MikkelRuby <mikkelruby1@hotmail.com>
2024-12-16 13:27:01 +0100

    r

--- src/pages/achievement.jsx
+++ src/pages/achievement.jsx
@@ -12,16 +12,16 @@ const Achievement = () => {
 
   // Ikoner og beskrivelser for præstationerne
   const icons = [
-    "runninman.png",
-    "medaljeikon.png",
-    "flagikon.png",
-    "benworkikon.png",
-    "stjerneikon.png",
-    "skubikon.png",
-    "fitnessliftikon.png",
-    "dumbellikon.png",
-    "stormedaljeikon.png",
-    "yogaikon.png",
+    "./runninman.png",
+    "./medaljeikon.png",
+    "./flagikon.png",
+    "./benworkikon.png",
+    "./stjerneikon.png",
+    "./skubikon.png",
+    "./fitnessliftikon.png",
+    "./dumbellikon.png",
+    "./stormedaljeikon.png",
+    "./yogaikon.png",
   ];
 
   const descriptions = [
